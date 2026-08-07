04a6ea1db184ca8ba074632daff0dd9b68af1eca
jambrocio-dev17 <sebasambrocio2005@gmail.com>
2026-08-06 22:34:49 -0600

    se hace clase de comparaacion para la ejecucuon que diga cual es el numero mayor de los 3 ingresos

---
+++ EJERCICIO 9/Comparador.java
@@ -0,0 +1,15 @@
+public class Comparador {
+    public int obtenerMayor(int a, int b, int c) {
+        int mayor = a;
+
+        if (b > mayor) {
+            mayor = b;
+        }
+
+        if (c > mayor) {
+            mayor = c;
+        }
+
+        return mayor;
+    }
+}
